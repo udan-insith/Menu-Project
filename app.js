5e5c4f4820e63d28e343d24618cf86d9816647ed
udan-insith <udaninsith23613@gmail.com>
2026-02-13 18:07:59 +0530

    adding method 2
    if need to add more buttons??

--- app.js
+++ app.js
@@ -71,6 +71,13 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+    id: 10,
+    title: "steak beef",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector('.section-center')
@@ -113,3 +120,9 @@ function displayMenuItems(menuItems) {
   displayMenu = displayMenu.join("")
   sectionCenter.innerHTML = displayMenu
 }
+
+// Method 2 - more suitable
+
+// get only unique categories - HARDEST ONE 
+// iterate over categories return buttons
+// make sure to select buttons when they are available
